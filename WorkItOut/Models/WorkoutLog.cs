0709cc256e7fe688b1db7ab1d8ca7a42d1f76b3a
Jaafar, Alaa <AlaaJaafar@quickenloans.com>
2021-07-06 12:02:24 -0400

    devbuild final

---
+++ WorkItOut/Models/WorkoutLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
+
+
+namespace WorkItOut.Models
+{
+    [Table("workoutlog")]
+    public class WorkoutLog
+    {
+        public int workoutlogId { get; set; }
+        public int weight { get; set; }
+        public DateTime timeOfWorkout { get; set; }
+        public int reps { get; set; }
+        public int sets { get; set; }
+        public int howDoYouFeel { get; set; }
+        public int exerciseId { get; set; }
+    }
+}
