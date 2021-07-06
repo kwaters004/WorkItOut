ed3c718148459e607ecd55b099e6373c1fc7f370
matthewmorris@quickenloans.com <matthewmorris@quickenloans.com>
2021-07-06 16:58:33 -0400

    controllers

---
+++ WorkItOut/Controllers/WorkItOutController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkItOut.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorkItOut.Controllers
+{
+    [Route("workout")]
+    [ApiController]
+    public class WorkOutsController : ControllerBase
+    {
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public List<Workouts> GetAllWorkOuts(int id)
+        {
+            return 3;
+            //DAL.GetAllWorkOuts();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public List<Workouts> GetSingleWorkOuts(int id)
+        {
+            return 3;
+            //DAL.GetAllWorkOuts();
+            //hello
+        }
+
+
+        // POST api/<ValuesController>
+        [HttpPost("add")]
+        public bool AddWorkout(string equipment, string category, string musclegroups)
+        {
+            //DAL.AddWorkout(equipment, category, musclegroups)
+            return true;
+        }
+
+        [HttpDelete("remove/{workoutsId}")]
+        public bool RemoveWorkOut(int id)
+        {
+            //DAL.removeWorkOut(id);
+            return true;
+        }
+    }
+}
