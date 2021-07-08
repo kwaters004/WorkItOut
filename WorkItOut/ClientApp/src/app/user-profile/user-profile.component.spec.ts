6923e113432fe3c18d4484efff27a47ccc3dbe99
Kirk Waters <kirkwaters@quickenloans.com>
2021-07-08 15:39:13 -0400

    redid the thing that was accidently deleted...oops

---
+++ WorkItOut/ClientApp/src/app/user-profile/user-profile.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { UserProfileComponent } from './user-profile.component';
+
+let component: UserProfileComponent;
+let fixture: ComponentFixture<UserProfileComponent>;
+
+describe('user-profile component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ UserProfileComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(UserProfileComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ WorkItOut/ClientApp/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user-profile',
+    templateUrl: './user-profile.component.html',
+    styleUrls: ['./user-profile.component.css']
+})
+/** user-profile component*/
+export class UserProfileComponent {
+    /** user-profile ctor */
+    constructor() {
+
+    }
+}
