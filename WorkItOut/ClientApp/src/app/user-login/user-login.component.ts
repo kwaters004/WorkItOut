6b4505bb7858a9a9bd05de9da47d01d04420d5b2
Jaafar, Alaa <AlaaJaafar@quickenloans.com>
2021-07-08 10:00:47 -0400

    user-signup component

--- WorkItOut/ClientApp/src/app/user-login/user-login.component.ts
+++ WorkItOut/ClientApp/src/app/user-login/user-login.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-user-login',
@@ -7,8 +7,24 @@
 })
 /** user-login component*/
 export class UserLoginComponent {
-    /** user-login ctor */
-    constructor() {
-
+/** user-login ctor */
+    login = {
+        firstname: "",
+        lastname: "",
+        dob: 0,
+        initialWeight: 0,
+        email: "",
+        userName: "",
+        height: 0,
+        gender: ""
     }
+
+
+  @Input() firstname = "";
+  constructor() {
+    
+  }
+    AddUser() {
+    alert('newuser');
+  }
 }
