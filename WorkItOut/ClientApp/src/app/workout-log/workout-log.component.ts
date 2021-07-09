792df183e85a2e2e95b1d07dc20ee73052413fbd
Verkeia Phillips <verkeiaphillips@quickenloans.com>
2021-07-09 17:24:16 -0400

    updated the workout log component

--- WorkItOut/ClientApp/src/app/workout-log/workout-log.component.ts
+++ WorkItOut/ClientApp/src/app/workout-log/workout-log.component.ts
@@ -11,7 +11,7 @@ export class WorkoutLogComponent {
 
     workoutlog = {
         weight: 0,
-        timeOfWorkout : 0,
+        /*timeOfWorkout : 0,*/
         reps : 0,
         sets: 0,
         howDoYouFeel: 0,
